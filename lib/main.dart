363a9e4eb65d4758e44ee20670932d292182af8c
sheraz-ali <soomrosheraz054@gmail.com>
2026-09-14 16:25:02 +0500

    Skin care app: Project structure set

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:skincare/constants/string_const.dart';
+import 'package:skincare/presentation/screens/welcome_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,12 +13,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: StringConst.appTitle,
       theme: ThemeData(
-      
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        brightness: .light
       ),
-      home: Scaffold(body: Center(child: Text("Skin care app"),),)
+      home: WelcomeScreen()
     );
   }
 }
